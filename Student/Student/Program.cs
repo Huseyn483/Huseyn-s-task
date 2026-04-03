5ac4604be3753904c6a16276a56eef05b0b21f63
Huseyn483 <huseynli2006huseyn@gmail.com>
2026-04-03 16:31:47 +0400

    student class

---
+++ Student/Student/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Student
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Student s = new Student();
+                s.Name = "Huseyn";
+                s.Age = 19;
+                s.ExamScore = 90;
+                s.PassingScore = 75;
+                s.Netice();
+            }
+        }
+    }
+}
---
+++ Student/Student/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+        public int ExamScore;
+        public int PassingScore;
+        public void Netice()
+        {
+            if (ExamScore >= PassingScore)
+            {
+                Console.WriteLine("Telebe kecdi");
+            }
+            else
+            {
+                int bal = PassingScore - ExamScore;
+                Console.WriteLine("Telebe kesildi");
+                Console.WriteLine("Kecmek ucun lazim olan bal: " + bal);
+            }
+        }
+    }
+}
